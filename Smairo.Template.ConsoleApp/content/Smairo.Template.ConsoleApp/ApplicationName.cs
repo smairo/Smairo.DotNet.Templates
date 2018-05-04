a891160d7eca057b694ccb750da96e4d1550d826
Miro Holopainen <smairo@outlook.com>
2018-05-04 17:43:10 +0300

    Console template added

---
+++ Smairo.Template.ConsoleApp/content/Smairo.Template.ConsoleApp/ApplicationName.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+namespace Smairo.Template.ConsoleApp
+{
+    public class ApplicationName : IApplicationName
+    {
+        private readonly ILogger<ApplicationName> _logger;
+        public ApplicationName(ILogger<ApplicationName> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task Run()
+        {
+            _logger.LogInformation("Hello world!");
+        }
+    }
+
+    public interface IApplicationName
+    {
+        Task Run();
+    }
+}
---
+++ Smairo.Template.ConsoleApp/content/Smairo.Template.ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿#region Using...
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+#endregion
+namespace Smairo.Template.ConsoleApp
+{
+    public class Program
+    {
+        public static IConfiguration Configuration { get; set; }
+
+        static async Task Main(string[] args)
+        {
+            // Build configuration
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            // Create application logger
+            var logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.Logentries(Configuration["LogentriesTokens:ApplicationName"])
+                .CreateLogger();
+
+            // Create service container
+            var services = new ServiceCollection()
+                .AddSingleton<IApplicationName, ApplicationName>()
+                .AddOptions()
+                .AddLogging(log => log.AddSerilog(logger, true))
+                .BuildServiceProvider();
+
+            // Get application and run
+            var application = services.GetService<IApplicationName>();
+            await application.Run();
+        }
+    }
+}
